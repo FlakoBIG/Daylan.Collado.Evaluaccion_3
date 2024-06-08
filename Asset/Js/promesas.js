dad1e2648d8cfe65ce5281a63824303220a0b69c
FlakoBIG <Collado_81@hotmail.cl>
2024-06-07 22:15:28 -0400

    Tabla de registrados
    Ahora la tabla muestra los soldados registrados en la base de datos, ademas de contener los botones de actializar y eliminar , pero aun siguen en proceso, ademas de alguno que otros cambios

--- Asset/Js/promesas.js
+++ Asset/Js/promesas.js
@@ -1,9 +1,18 @@
-import {collection} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import {collection,addDoc,getDocs,doc} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import {db} from "./firebase.js";
 
-//esto es para registrar soldados a la base de datos
-export const RegistrarRecluta = async(Recluta)=>{
+//esto es para registrar Reclutas a la base de datos
+export const RegistrarSoldado = async(Recluta)=>{
     const docRef = await addDoc(collection(db,"Recluta"),Recluta);
 }
-//esto para obtener los soldados que se registraron en la base de datos
-export const obtenerReclutados = async()=>{}
+//esto para obtener los Reclutas que se registraron en la base de datos
+export const obtenerReclutados = async()=>{
+    const ref = collection(db,"Recluta");
+    const querySnap = await getDocs(ref);
+    let lista = [];
+    querySnap.forEach(doc =>{
+        //lee cada uno de los documentos que estan en firebase
+        lista.push({...doc.data(),id:doc.id});
+    });
+    return lista
+}
--- Asset/Js/script.js
+++ Asset/Js/script.js
@@ -1,16 +1,18 @@
-import { RegistrarRecluta, obtenerReclutados } from "./promesas.js";
+import { RegistrarSoldado, obtenerReclutados } from "./promesas.js";
 //Esto es para cuando la pagina cargue completamente se ejecute lo que este dentro
 window.addEventListener("load",()=>{
-    document.getElementById("BTNRegistrar").addEventListener("click",RegistrarSoldado);
+    document.getElementById("BTNRegistrar").addEventListener("click",RegistrarRecluta);
+    //se cargan los registrados
+    CargarReclutas();
     
 });
 //tengo pensado asta ahora , primero pasar por las validaciones y que las propias validaciones le entreguen a el registrar , pero voy a empesar registrando ya que es lo 
 //mas tardido , luego adaptare
 //const Validaciones = ()=>{}
 
-const RegistrarSoldado = ()=>{
+const RegistrarRecluta = ()=>{
     //esto es para ver si entramos
-    console.log("Entre en registrarsoldado")
+    console.log("Entre en registrarReclutas")
     //Primero se recuperan los elementos , por eso el nombre "ENombre" = ElementoNombre
     let ENombre = document.getElementById("INPNombre");
     let EApellido_1 = document.getElementById("INPApellido_1");
@@ -40,10 +42,12 @@ const RegistrarSoldado = ()=>{
     let VMensaje = EMensaje.value;
     let VSi = ESi.value;
     let VNo = ENo.value;
-    Manejo_armas="";
+    let Manejo_armas="";
     if (VSi){Manejo_armas="Si"}else{Manejo_armas="No"}
     //Creamos la lista para los vehiculos que maneja
-    Lista_de_vehiculos = [];
+    let Lista_de_vehiculos = [];
+    //ahora le metemos los vehiculos
+    // si Vauto es true, a la lista se le integra "Auto"
     if (VAuto){Lista_de_vehiculos.push("Auto")};
     if (VMoto){Lista_de_vehiculos.push("Moto")};
     if (VAvion){Lista_de_vehiculos.push("Avion")};
@@ -51,38 +55,44 @@ const RegistrarSoldado = ()=>{
     //revisamos la lista en consola
     console.log(Lista_de_vehiculos);
     //ahora se crea el objeto
-    let Soldado = {Nombre:VNombre,Apellido_1:VApellido_1,Apellido_2:VApellido_2,Rut:VRut,Fecha_de_nacimiento:VFecha,
+    let Recluta = {Nombre:VNombre,Apellido_1:VApellido_1,Apellido_2:VApellido_2,Rut:VRut,Fecha_de_nacimiento:VFecha,
         Ultimo_Estudio_cursado:VEstudio_cursado,Maneja:Lista_de_vehiculos,Quire_entrar_porque:VMensaje,Manejo_de_armas:Manejo_armas}
-    console.log(Soldado)
-    RegistrarRecluta(Soldado).then(()=>{
-        console.log("El soldado se registro con exito")
+    console.log(Recluta)
+    //el then en este caso estaria imprimiendo el mensje "el Recluta...", 
+    //pero mas que nada es para decir que se ejecuto bien
+    RegistrarSoldado(Recluta).then(()=>{
+        console.log("El Recluta se registro con exito")
+        CargarReclutas();
+    //el catch es para si algo se ejecuta mal mostrara ese mensaje
     }).catch((error)=>{
         alert("Algo paso , no se pudo registrar :c")
         console.log(error);
     })
 }
 
-//Ahora se cargara los soldados que estan registrados en la base
-const CargarSoldados = ()=>{
-    obtenerReclutados().then((Soldado)=>{
-        console.log("Se recupero un soldado");
-        console.log(Soldado);
+//Ahora se cargara los Reclutas que estan registrados en la base
+const CargarReclutas = ()=>{
+    obtenerReclutados().then((Recluta)=>{
+        console.log("Se recupero un Recluta");
+        console.log(Recluta);
+        //ahora se prepara la estructura para la tabla
+        let Estructura = "";
+        Recluta.forEach((Soldado)=>{
+            Estructura += "<tr>";
+            Estructura += "<td>"+Soldado.Nombre+"</td>";
+            Estructura += "<td>"+Soldado.Apellido_1+"</td>";
+            Estructura += "<td>"+Soldado.Apellido_2+"</td>";
+            Estructura += "<td>"+Soldado.Rut+"</td>";
+            Estructura += "<td>"+Soldado.Fecha_de_nacimiento+"</td>";
+            Estructura += "<td>"+Soldado.Ultimo_Estudio_cursado+"</td>";
+            Estructura += "<td>"+Soldado.Maneja+"</td>";
+            Estructura += "<td>"+Soldado.Quire_entrar_porque+"</td>";
+            Estructura += "<td>"+Soldado.Manejo_de_armas+"</td>";
+            Estructura += "<td><button>"+"Actualizar</button><button>"+"Eliminar</button></td>"
+            Estructura += "</tr>";
+        });
+        console.log(Estructura);
+        //Este modifica el html para meterle la estrutura
+        document.getElementById("Tabla_de_Reclutados").innerHTML = Estructura;
     })
-    //ahora se prepara la estructura para la tabla
-    let Estructura = "";
-    Soldado.forEach(recluta => {
-        Estructura += "<tr>";
-        Estructura += "<td>"+recluta.Nombre+"</td>";
-        Estructura += "<td>"+recluta.Apellido_1+"</td>";
-        Estructura += "<td>"+recluta.Apellido_2+"</td>";
-        Estructura += "<td>"+recluta.Rut+"</td>";
-        Estructura += "<td>"+recluta.Fecha_de_nacimiento+"</td>";
-        Estructura += "<td>"+recluta.Ultimo_Estudio_cursado+"</td>";
-        Estructura += "<td>"+recluta.Maneja+"</td>";
-        Estructura += "<td>"+recluta.Quire_entrar_porque+"</td>";
-        Estructura += "<td>"+recluta.Manejo_de_armas+"</td>";
-        Estructura += "</tr>";
-    });
-    console.log(Estructura)
-
 }
